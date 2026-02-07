132076d890507fb899dfef315c49069f2a36294b
cheralynli <cheralynlianto@gmail.com>
2026-02-07 09:56:06 +0800

    update on cpp file

--- pangram.cpp
+++ pangram.cpp
@@ -4,24 +4,30 @@
 #include <string>
 #include <iostream>
 #include <array>
+#include <cctype>
 
 namespace pangram {
 
-bool is_pangram(std::string x){
+bool is_pangram(const std::string& x) {  // Use const reference
     bool letter[26] = {false};
-    for (char k : x){
-        if (k>='a' && k<='z'){
-            letter[k-'a']=true;
-        }
-        if (k>='A' && k<='Z'){
-            letter[k-'A']=true;
+    
+    for (char k : x) {
+        if (std::isalpha(static_cast<unsigned char>(k))) {
+            char lower_k = std::tolower(static_cast<unsigned char>(k));
+            if (lower_k >= 'a' && lower_k <= 'z') {
+                letter[lower_k - 'a'] = true;
+            }
         }
     }
-    for (int i=0; i<26 ; i++){
-        if (letter[i]==false) {return false;}
+    
+    for (int i = 0; i < 26; i++) {
+        if (!letter[i]) {
+            return false;
+        }
     }
     return true;
 }
+
 void check_pangram() {    
     std::string sentence;
     std::cout << "Enter a sentence: ";
@@ -32,4 +38,6 @@ void check_pangram() {
     } else {
         std::cout << "Not pangram" << std::endl;
     }
+}
+
 }  // namespace pangram
